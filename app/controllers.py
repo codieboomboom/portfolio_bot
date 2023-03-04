8843e8d9629a4386bde86da85cd93ff72fab06ff
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-04 18:46:13 +0800

    Remove session open/close for ORM as we are using flask-sqlalchemy extension instead of raw sqlalchemy

--- app/controllers.py
+++ app/controllers.py
@@ -4,13 +4,11 @@ from app.Model import Stock, Crypto, MutualFund
 
 def add_asset(chat_id, symbol, quantity, add_mode, market=None):
     if add_mode == "ADD_STOCKS":
-        db.session()
         existing_stock_symbol_in_portfolio = (
             session.query(Stock).filter_by(chat_id=chat_id).first()
         )
         if existing_stock_symbol_in_portfolio:
             # TODO: A wrapper Error around the message so it is easier
-            db.session.close()
             return "Cannot add existing symbol in portfolio to it. Use /update instead"
         # TODO: Validate if ticker exist in market specified using some HTTPs
         stock_to_be_added = Stock(
@@ -18,7 +16,6 @@ def add_asset(chat_id, symbol, quantity, add_mode, market=None):
         )
         db.session.add(stock_to_be_added)
         db.session.commit()
-        db.session.close()
     elif add_mode == "ADD_CRYPTOS":
         pass
     elif add_mode == "ADD_FUNDS":
@@ -29,16 +26,13 @@ def add_asset(chat_id, symbol, quantity, add_mode, market=None):
 
 def delete_asset(chat_id, symbol, delete_mode):
     if delete_mode == "DELETE_STOCKS":
-        db.session()
         existing_stock_symbol_in_portfolio = (
             session.query(Stock).filter_by(chat_id=chat_id).first()
         )
         if not existing_stock_symbol_in_portfolio:
-            db.session.close()
             return "No symbol found. Cannot be deleted."
         db.session.delete(existing_stock_symbol_in_portfolio)
         db.session.commit()
-        db.session.close()
     elif delete_mode == "DELETE_CRYPTOS":
         pass
     elif delete_mode == "DELETE_FUNDS":
