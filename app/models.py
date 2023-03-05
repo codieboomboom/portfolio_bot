dd64ed6b589b9ebb88d88ed9e9537a94d58b2686
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-05 10:28:27 +0800

    Added to_string for models and adapt for flask-sqlalchemy

--- app/models.py
+++ app/models.py
@@ -2,28 +2,31 @@ from app import db
 
 
 class Stock(db.Model):
-    __tablename__ = "stocks"
-
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer)
     symbol = db.Column(db.String)
     quantity = db.Column(db.Integer)
     market = db.Column(db.String)
 
+    def __repr__(self):
+        return f"Stock {self.symbol}"
+
 
 class Crypto(db.Model):
-    __tablename__ = "cryptos"
-
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer)
     symbol = db.Column(db.String)
     quantity = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Crypto {self.symbol}"
 
 
 class MutualFund(db.Model):
-    __tablename__ = "mutualfunds"
-
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer)
     symbol = db.Column(db.String)
     quantity = db.Column(db.Float)
+
+    def __repr__(self):
+        return f"Mutual Funds {self.symbol}"
