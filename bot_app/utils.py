e835410628fe4d0ccb1ce4604c648bf904190451
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-08 00:03:58 +0800

    Fix send_message error due to wrong payload format (expecting reply_markup all the time)

--- bot_app/utils.py
+++ bot_app/utils.py
@@ -5,11 +5,13 @@ from flask import current_app
 # Send message to Telegram chat with chat_id
 # TODO: Add logging here
 def send_message(chat_id, text, reply_markup=None):
-    url = current_app.config['TELEGRAM_BOT_BASE_URL'] + "/sendMessage"
-    payload = {"chat_id": chat_id, "text": text, "reply_markup": reply_markup}
+    url = current_app.config["TELEGRAM_BOT_BASE_URL"] + "/sendMessage"
+    if reply_markup:
+        payload = {"chat_id": chat_id, "text": text, "reply_markup": reply_markup}
+    else:
+        payload = {"chat_id": chat_id, "text": text}
     response = requests.post(url, json=payload)
     if response.status_code != 200:
-        # TODO: use logger instead of print?
-        print(f"Error sending message to {chat_id}: {response.text}")
+        current_app.logger.info(f"Error sending message to {chat_id}: {response.text}")
 
     return "Done"
