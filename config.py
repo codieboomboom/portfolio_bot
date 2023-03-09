42ce6fe07c36662a1622e2d48267a8f4969cf0b1
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-09 14:02:39 +0000

    Slight modification for configs

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class DevConfig:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret-key"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    ) or "sqlite:///" + os.path.join(basedir, "dev.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN") or "unknown"
     TELEGRAM_BOT_BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
