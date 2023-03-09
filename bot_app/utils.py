e25a500faa81c117fcf30f92f5470592610422e2
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-09 03:17:51 +0000

    Added logging for send_message

--- bot_app/utils.py
+++ bot_app/utils.py
@@ -3,13 +3,14 @@ from flask import current_app
 
 
 # Send message to Telegram chat with chat_id
-# TODO: Add logging here
 def send_message(chat_id, text, reply_markup=None):
     url = current_app.config["TELEGRAM_BOT_BASE_URL"] + "/sendMessage"
+    current_app.logger.debug(f"URL for sending: {url}")
     if reply_markup:
         payload = {"chat_id": chat_id, "text": text, "reply_markup": reply_markup}
     else:
         payload = {"chat_id": chat_id, "text": text}
+    current_app.logger.debug(f"Payload: {payload}")
     response = requests.post(url, json=payload)
     if response.status_code != 200:
         current_app.logger.info(f"Error sending message to {chat_id}: {response.text}")
