8ffe28fc27788ba10e8da7f9f065cec67a4591e9
Do Anh Tu <anhtu.do1998@gmail.com>
2023-03-13 06:35:16 +0000

    Added dynamodb init in __init__.py

--- config.py
+++ config.py
@@ -26,8 +26,12 @@ class TestConfig:
 
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "production.db")
+    # Set up DynamoDB client
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') or 'youraccessid'
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') or 'yoursecretaccesskey'
+    DYNAMODB_REGION_NAME = os.environ.get('DYNAMODB_REGION_NAME') or 'ap-southeast-1'
+    DYNAMODB_TABLE_NAME = 'assets'
+    SQLALCHEMY_DATABASE_URI = f'dynamodb://{DYNAMODB_TABLE_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN") or "unknown"
     TELEGRAM_BOT_BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
